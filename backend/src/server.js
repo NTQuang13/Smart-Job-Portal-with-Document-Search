d5eefe689f8a428e152fa72335c27928c6198ecd
Nguyễn Trường Quang <nguyentruongquang13@gmail.com>
2026-03-28 10:29:51 +0700

    Modify auth API routes for registration and login
    Updated authentication API routes to use signup and signin.

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Gắn các routers vào các endpoint tương ứng
-app.use("/api/auth", authRoutes); // Các api đăng ký/đăng nhập sẽ nằm ở /api/auth/register, /api/auth/login
+app.use("/api/auth", authRoutes); // Các api đăng ký/đăng nhập sẽ nằm ở /api/auth/signup, /api/auth/signin, /api/auth/signout
 app.use("/api/users", userRoutes); // Các api user sẽ nằm ở /api/users/me, /api/users/:id
 
 app.listen(PORT, () => {
